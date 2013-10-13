03d4f955f8e1c20e0f93002c888cd7473a866ce2
Pavlo Manovi <pavlo.manovi@gmail.com>
2013-10-13 00:54:44 -0700

    Initial commit, added basis of code to work off of.

---
+++ HappyBirthdayMom.X/main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: Pavlo
+ *
+ * Created on October 12, 2013, 11:36 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+	return(EXIT_SUCCESS);
+}
+
